5740318ad49d0226fec08b8fe1c1c396d5b612a3
Kgaogelothorn <kgaogelothorn@gmail.com>
2019-11-02 22:42:06 +0200

    refactor(header): added a header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TracksComponent } from './components/tracks/tracks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'tracks', pathMatch: 'full'},
+  { path: 'tracks', component: TracksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
